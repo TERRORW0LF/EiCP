df2a7ffee1c1061751a6a9a27894742017acabbd
Finn Brandt <finn2003minicooper@gmail.com>
2024-03-09 21:21:57 +0100

    feat: basic shader

--- src/config.h
+++ src/config.h
@@ -6,3 +6,7 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include "config.h"
 
-// Most of the window setup is based upon GetIntoGameDev's OpenGL tutorial
+// Declare function prototypes to avoid sorting them in code.
+unsigned int make_shader(const std::string &vertex_filepath, const std::string &fragment_filepath);
+unsigned int make_module(const std::string &filepath, unsigned int module_type);
+
+// Most of the window and OpenGL setup is based upon GetIntoGameDev's OpenGL tutorial
 // at https://www.youtube.com/watch?list=PLn3eTxaOtL2PHxN8EHf-ktAcN-sGETKfw
 int main()
 {
@@ -38,6 +42,9 @@ int main()
     // Set the background color of the window.
     glClearColor(0.25f, 0.5f, 0.75f, 1.0f);
 
+    // Create a shader for the objects in the scene.
+    unsigned int shader = make_shader("src/shaders/vertex.txt", "src/shaders/fragment.txt");
+
     // Window event loop. Runs until the user closes the window.
     while (!glfwWindowShouldClose(window))
     {
@@ -48,11 +55,111 @@ int main()
         // This sets the color to the one defined by glClearColor.
         glClear(GL_COLOR_BUFFER_BIT);
 
+        // Use the shader in the new buffer.
+        glUseProgram(shader);
+
         // Gives the window the new buffer updated with glClear.
         glfwSwapBuffers(window);
     }
+    // Delete shader program before terminating.
+    glDeleteProgram(shader);
 
     // Close the window and return ok status code.
     glfwTerminate();
     return 0;
+}
+
+/**
+ * Creates an OpenGL shader from given file paths.
+ * The file paths must be relative to the current working directory.
+ *
+ * @param vertex_filepath The path to the vertex shader file.
+ * @param fragement_filepath The path to the fragment shader file.
+ * @returns an OpenGL shader.
+ *  @brief Creates an OpenGL shader from a given filepath.
+ */
+unsigned int make_shader(const std::string &vertex_filepath, const std::string &fragment_filepath)
+{
+    // Create a shader and relevant shader modules.
+    unsigned int shader = glCreateProgram();
+    unsigned int vertex_shader = make_module(vertex_filepath, GL_VERTEX_SHADER);
+    unsigned int fragment_shader = make_module(fragment_filepath, GL_FRAGMENT_SHADER);
+
+    // Link modules to shader.
+    glAttachShader(shader, vertex_shader);
+    glAttachShader(shader, fragment_shader);
+    glLinkProgram(shader);
+
+    // Check linking status and print error if linking failed.
+    // This might throw a segfault if errorLog is longer than 1024 chars.
+    int success;
+    glGetShaderiv(shader, GL_LINK_STATUS, &success);
+    if (!success)
+    {
+        char errorLog[1024];
+        glGetProgramInfoLog(shader, 1024, NULL, errorLog);
+        std::cout << "Shader linking error:\n"
+                  << errorLog << std::endl;
+    }
+
+    // Delete individual shader modules as we don't need them after linking.
+    glDeleteShader(vertex_shader);
+    glDeleteShader(fragment_shader);
+
+    return shader;
+}
+
+/**
+ * Creates an OpenGL shader module from a given file path.
+ * The file path must be relative to the current working directory.
+ * If the shader compilation fails a warning will be logged and the
+ * faulty module returned.
+ *
+ * @param filepath The path to the shader file.
+ * @param module_type The type of shader module to create.
+ * @returns an OpenGL shader module.
+ *  @brief Creates an OpenGL shader module from a given filepath.
+ */
+unsigned int make_module(const std::string &filepath, unsigned int module_type)
+{
+    // Setup streams for reading shader module file.
+    std::ifstream file;
+    std::stringstream bufferedLines;
+    std::string line;
+
+    // Try to open the shader file. Print warning if that failed.
+    file.open(filepath);
+    if (!file.is_open())
+    {
+        std::cout << "Unable to open shader file." << std::endl;
+    }
+
+    // Read file and convert it into a char array.
+    while (std::getline(file, line))
+    {
+        bufferedLines << line << "\n";
+    }
+
+    std::string shaderSource = bufferedLines.str();
+    const char *shaderSrc = shaderSource.c_str();
+    file.close();
+
+    // Compile shader module
+    unsigned int shaderModule = glCreateShader(module_type);
+    glShaderSource(shaderModule, 1, &shaderSrc, NULL);
+    glCompileShader(shaderModule);
+
+    // Check compilation status and print error if compilation failed.
+    // This might throw a segfault if errorLog is longer than 1024 chars.
+    int success;
+    glGetShaderiv(shaderModule, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        char errorLog[1024];
+        glGetShaderInfoLog(shaderModule, 1024, NULL, errorLog);
+        std::cout << "Shader Module compilation error:\n"
+                  << errorLog << std::endl;
+    }
+
+    return shaderModule;
 }
