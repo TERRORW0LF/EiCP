3d298327583d2085367f3b093fc59b9659f7b566
Finn Brandt <finn2003minicooper@gmail.com>
2024-03-09 19:46:59 +0100

    docs: add documentation

--- src/config.h
+++ src/config.h
@@ -1,5 +1,8 @@
+// This ensures that the file will only be included once
+// by throwing out all subsequent inclusions during compilation.
 #pragma once
 
+// Collection of all relevant includes in the main programm.
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,52 @@
 #include "config.h"
 
+// Most of the window setup is based upon GetIntoGameDev's OpenGL tutorial
+// at https://www.youtube.com/watch?list=PLn3eTxaOtL2PHxN8EHf-ktAcN-sGETKfw
 int main()
 {
+    // The current and only window used in this application.
     GLFWwindow *window;
 
+    // Initialize the window management framework glfw.
+    // If initialization fails, abort execution with error code.
     if (!glfwInit())
     {
         std::cout << "GLFW couldn't be initialized";
         return -1;
     }
 
+    // Create the window and set it as the currently active window.
     window = glfwCreateWindow(640, 480, "XPBD Cloth simulation", NULL, NULL);
     glfwMakeContextCurrent(window);
 
+    // Dynamically load the OpenGL libraries.
+    // This is done via glad, generated for the core OpenGL functionality
+    // and OpenGL version 4.6.
+    // Abort with error code if OpenGL libraries couldn't be found.
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         glfwTerminate();
         return -1;
     }
+
+    // Set the background color of the window.
     glClearColor(0.25f, 0.5f, 0.75f, 1.0f);
 
+    // Window event loop. Runs until the user closes the window.
     while (!glfwWindowShouldClose(window))
     {
+        // Poll all events for this frame.
         glfwPollEvents();
+
+        // Clear the background color buffer.
+        // This sets the color to the one defined by glClearColor.
         glClear(GL_COLOR_BUFFER_BIT);
+
+        // Gives the window the new buffer updated with glClear.
         glfwSwapBuffers(window);
     }
 
+    // Close the window and return ok status code.
     glfwTerminate();
     return 0;
 }
