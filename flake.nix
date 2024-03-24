156a7a2037993ef0de656e275ef5a068395cf77c
Finn Brandt <finn2003minicooper@gmail.com>
2024-03-24 23:05:55 +0100

    feat: nix setup

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  description = "C/C++ environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils, ... }@inputs:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        p = import nixpkgs { 
+				inherit system;
+				config = { 
+					allowUnfree = true; 
+				}; 
+			};
+        llvm = p.llvmPackages_latest;
+      in
+      {
+        devShell = p.mkShell.override { stdenv = p.clangStdenv; } rec {
+          packages = with p; [
+			# builder
+			cmake
+
+			# debugger
+			llvm.lldb
+			gdb
+
+			# fix headers not found
+			clang-tools
+
+			# LSP and compiler
+			llvm.libstdcxxClang
+
+			# other tools
+			llvm.libllvm
+
+			# stdlib for cpp
+			llvm.libcxx
+				
+			# libs
+			libGL
+			glfw
+			mesa.dev
+          ];
+          name = "C";
+        };
+      }
+    );
+}
